6d05a168f95dcd823912625a1bb1b0134d1c3395
867380699 <867380699@qq.com>
2021-08-02 23:28:36 +0800

    feat: txt -> epub

--- rss.py
+++ rss.py
@@ -1,7 +1,8 @@
 import os
 import time
-import feedparser
 from pathlib import Path
+import feedparser
+from ebooklib import epub
 
 feedUri = 'http://127.0.0.1:1200/readhub/category/topic'
 filepath = str(Path.home()) + '/readhub/txt/'
@@ -14,14 +15,39 @@ h = int(time.strftime('%H'))
 h_text = '早' if h < 12 else '午' if h < 18 else '晚'
 c_now = time.strftime('%Y年%m月%d日')
 
-filename = "Readhub-%s%s.txt" % (c_now.replace(' ', ''), h_text)
+title = f'{c_now} {h_text}'
 
-c_file = open(filepath + filename, 'w')
+filename = f"Readhub-{title.replace(' ', '')}.epub"
 
-c_file.write('%s %s\n\n' % (c_now, h_text))
+book = epub.EpubBook()
+
+book.set_title(title)
+book.set_language('zh-CN')
+book.add_author('readhub')
+book.add_item(epub.EpubNcx())
+book.add_item(epub.EpubNav())
+
+style = """
+BODY { text-align: justify; }
+h1,h2 { margin-bottom: 1em }
+"""
+
+default_css = epub.EpubItem(uid='style_default', file_name="style/default.css", media_type="text/css", content=style)
+
+c1 = epub.EpubHtml(title="feed", file_name="feed.xhtml")
+c1.set_language('zh-CN')
+c1.add_item(default_css)
+
+c1.content=f'<h1>{title}</h1>'
+
 for s in summary:
-    c_file.write('%s\n\n' % s['title'])
+    c1.content += f'<h2>{s["title"]}</h2>'
 
-c_file.close()
+book.add_item(c1)
+book.add_item(default_css)
+book.spine = ['nav', c1]
+book.toc = ((epub.Link('feed.xhtml', f'{title}', 'feed'),))
+
+epub.write_epub(f'{filepath}{filename}', book, {})
 
 os.system(f'termux-notification -t {filename.split(".")[0]} --action "termux-share -d {filepath}{filename}"')
